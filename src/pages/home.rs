74dfe6609f0bef262c25662564dddfe25b2303fc
Cory Buecker <email@corybuecker.com>
2025-03-02 09:23:31 -0600

    --wip-- [skip ci]

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -10,25 +10,6 @@ use mongodb::options::FindOptions;
 use std::collections::VecDeque;
 use tracing::error;
 
-pub struct AppError(anyhow::Error);
-
-impl From<anyhow::Error> for AppError {
-    fn from(err: anyhow::Error) -> Self {
-        AppError(err)
-    }
-}
-
-impl IntoResponse for AppError {
-    fn into_response(self) -> axum::response::Response {
-        error!("{}", self.0);
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            "Something has gone wrong.",
-        )
-            .into_response()
-    }
-}
-
 pub async fn build_response(
     State(shared_state): State<SharedState>,
 ) -> Result<Html<String>, AppError> {
