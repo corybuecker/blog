9637d86e350b5aac1fafdf27b00eb03632b94ab4
Cory Buecker <email@corybuecker.com>
2025-03-04 18:12:18 -0600

    feat: replace mongo with postgres
    closes #588

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,40 +1,46 @@
 use crate::types::{AppError, Page, SharedState};
 use anyhow::Context;
 use axum::{extract::State, response::Html};
-use bson::doc;
-use mongodb::options::FindOptions;
-use std::collections::VecDeque;
+use serde::Serialize;
+use std::{collections::VecDeque, sync::Arc};
+
+#[derive(Serialize, Debug)]
+struct Link {
+    title: String,
+    slug: String,
+}
 
 pub async fn build_response(
-    State(shared_state): State<SharedState>,
+    State(shared_state): State<Arc<SharedState>>,
 ) -> Result<Html<String>, AppError> {
     let tera = &shared_state.tera;
-    let mongo = shared_state
-        .mongo
-        .database("blog")
-        .collection::<Page>("pages");
+    let client = &shared_state.client;
+    let pages = client
+        .query(
+            "SELECT title, slug FROM pages WHERE published_at IS NOT NULL ORDER BY published_at DESC",
+            &[],
+        )
+        .await
+        .context("could not fetch pages")?;
+    let homepage: Page = client
+        .query_one(
+            "SELECT * FROM pages WHERE published_at IS NOT NULL ORDER BY published_at DESC LIMIT 1",
+            &[],
+        )
+        .await
+        .context("could not fetch homepage")?
+        .try_into()?;
 
     let mut context = tera::Context::new();
-    let find_options = FindOptions::builder()
-        .sort(doc! {"published_at": -1})
-        .build();
+    let mut pages: VecDeque<Link> = pages
+        .into_iter()
+        .map(|row| Link {
+            title: row.get("title"),
+            slug: row.get("slug"),
+        })
+        .collect();
 
-    let mut cur = mongo
-        .find(doc! {"published_at": doc!{"$lte": mongodb::bson::DateTime::now()}})
-        .with_options(find_options)
-        .await
-        .context("database error")?;
-
-    let mut pages: VecDeque<Page> = VecDeque::new();
-
-    while cur.advance().await.context("database error")? {
-        let page = cur.deserialize_current().context("database error")?;
-        pages.push_back(page)
-    }
-
-    let homepage = pages
-        .pop_front()
-        .context("cannot render a homepage without any pages")?;
+    pages.pop_front();
 
     context.insert("pages", &pages);
     context.insert("homepage", &homepage);
