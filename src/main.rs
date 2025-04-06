52e6e15f20c2aa5d4e56b385d7367072a3e00c75
Cory Buecker <email@corybuecker.com>
2025-04-06 06:05:01 -0500

    feat: add metrics via OpenTelemetry (#614)
    closes #613

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,52 @@
-use axum::{Router, routing::get};
+use axum::{
+    Router,
+    extract::Request,
+    middleware::{Next, from_fn},
+    response::IntoResponse,
+    routing::get,
+};
+use opentelemetry::{KeyValue, global};
 use std::sync::Arc;
 use tera::Tera;
 use tokio::signal::unix::SignalKind;
 use tokio_postgres::{NoTls, connect};
 use tower_http::{services::ServeDir, trace::TraceLayer};
-use tracing::{Level, info};
+use tracing::info;
 use types::SharedState;
-use utils::tera::{digest_asset, embed_templates};
+use utils::{
+    initialize_tracing,
+    tera::{digest_asset, embed_templates},
+};
 
 mod admin;
 mod pages;
 mod types;
 mod utils;
 
+async fn metrics(request: Request, next: Next) -> impl IntoResponse {
+    let start = std::time::Instant::now();
+    let path = request.uri().to_string();
+
+    let response = next.run(request).await;
+
+    let meter = global::meter("blog");
+    meter.f64_counter("latency").build().add(
+        start.elapsed().as_secs_f64(),
+        &[KeyValue::new("path", path)],
+    );
+
+    response
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing::subscriber::set_global_default(
-        tracing_subscriber::FmtSubscriber::builder()
-            .with_max_level(Level::DEBUG)
-            .finish(),
-    )?;
-
+    let metric_provider = initialize_tracing()?;
     let mut tera = Tera::default();
 
     tera.register_function("digest_asset", digest_asset());
     embed_templates(&mut tera).map_err(|e| anyhow::anyhow!("Failed to embed templates: {}", e))?;
 
     let database_url = std::env::var("DATABASE_URL")?;
-
     let (client, connection) = connect(&database_url, NoTls).await?;
 
     tokio::spawn(connection);
@@ -42,12 +61,14 @@ async fn main() -> anyhow::Result<()> {
         .nest_service("/images", ServeDir::new("static/images"))
         .nest("/admin", admin::admin_routes(shared_state.clone()))
         .with_state(shared_state)
-        .layer(TraceLayer::new_for_http());
+        .layer(TraceLayer::new_for_http())
+        .layer(from_fn(metrics));
 
-    let shutdown_signal = async {
+    let shutdown_signal = async move {
         let mut signal = tokio::signal::unix::signal(SignalKind::terminate())
             .expect("failed to install SIGTERM handler");
         signal.recv().await;
+        metric_provider.shutdown().unwrap();
         info!("Received shutdown signal, gracefully shutting down");
     };
 
